216c6db33cf453f75f70d2c14b61de3ea547b458
maurya512 <patelmaurya0512@gmail.com>
2021-08-01 18:30:12 -0400

    Added a rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 
+// import Rating Component
+import Rating from '../components/Rating'
+
 // Product component that takes in prd as a prop
 const Product = ({ prd }) => {
     return (
@@ -23,9 +26,12 @@ const Product = ({ prd }) => {
                 {/* displays the review for each product */}
                 {/* each text is brought in as a div so that it can be viewed as a separate form of data */}
                 <Card.Text as="div">
-                    <div className="my-3">
+                    {/* <div className="my-3">
                         {prd.rating} from {prd.numReviews} reviews
-                    </div>
+                    </div> */}
+
+                    {/* Rating component will take in 2 props, the rating and the number of reviews in the original component */}
+                    <Rating value={prd.rating} text={`${prd.numReviews} reviews`} />
                 </Card.Text>
 
                 {/* displays the price for each product */}
