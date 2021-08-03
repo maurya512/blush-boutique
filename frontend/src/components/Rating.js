36710734379f5add842081fa0bc1b51e3c9cb389
maurya512 <patelmaurya0512@gmail.com>
2021-08-02 23:12:06 -0400

    changed the color for rating stars to yellow

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -2,29 +2,29 @@ import React from 'react'
 
 // Rating component that displays the rating of each product based on the number or reviews they have
 // component takes in two props: value for rating, text for numReviews
-const Rating = ({ value, text }) => {
+const Rating = ({ value, text, starColor }) => {
     return (
         <div className="rating">
             {/* represents each rating with an icon */}
             <span>
                 {/* checks for 1 star reviews */}
-                <i className={value >= 1 ? 'fas fa-star' : value >= 0.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
+                <i style={{ color: starColor }} className={value >= 1 ? 'fas fa-star' : value >= 0.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
             </span>
             <span>
                 {/* checks for 2 star reviews */}
-                <i className={value >= 2 ? 'fas fa-star' : value >= 1.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
+                <i style={{ color: starColor }} className={value >= 2 ? 'fas fa-star' : value >= 1.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
             </span>
             <span>
                 {/* checks for 3 star reviews */}
-                <i className={value >= 3 ? 'fas fa-star' : value >= 2.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
+                <i style={{ color: starColor }} className={value >= 3 ? 'fas fa-star' : value >= 2.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
             </span>
             <span>
                 {/* checks for 4 star reviews */}
-                <i className={value >= 4 ? 'fas fa-star' : value >= 3.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
+                <i style={{ color: starColor }} className={value >= 4 ? 'fas fa-star' : value >= 3.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
             </span>
             <span>
                 {/* checks for 5 star reviews */}
-                <i className={value >= 5 ? 'fas fa-star' : value >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
+                <i style={{ color: starColor }} className={value >= 5 ? 'fas fa-star' : value >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
             </span>
 
             {/* text  */}
@@ -35,5 +35,19 @@ const Rating = ({ value, text }) => {
         </div>
     )
 }
+
+// set a default value for the color stars in the rating component
+// if a color is specified the component will 
+Rating.defaultProps = {
+    starColor: '#f8e825',
+}
+
+
+// set a default type of prop passed
+// Rating.propTypes = {
+//     value: propTypes.number.isRequired,
+//     text: propTypes.string.isRequired,
+//     color: propTypes.number,
+// }
 
 export default Rating
