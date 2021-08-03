8685b4ae18c449cbb9d8c7a96986e787bf850245
maurya512 <patelmaurya0512@gmail.com>
2021-08-02 23:11:20 -0400

    changed from anchor tags to LinkContainer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+// LinkContainer serves the same purpose as regular Link. Just used to bind bootstrap containers. 
+import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,12 +8,18 @@ const Header = () => {
         <header>
             <Navbar bg="light" variant="light" expand="lg">
                 <Container>
-                    {/* redirects the user to the home page when clicked */}
-                    <Navbar.Brand href="/home">Blush Boutique</Navbar.Brand>
+                    <LinkContainer to="/">
+                        {/* redirects the user to the home page when clicked */}
+                        <Navbar.Brand>Blush Boutique</Navbar.Brand>
+                    </LinkContainer>
                     <Nav className="ml-auto">
                         {/* nav links that redirect the user to their respective components */}
-                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-                        <Nav.Link href="/login"><i className="fas fa-user"></i> Login</Nav.Link>
+                        <LinkContainer to="/cart">
+                            <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link><i className="fas fa-user"></i> Login</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Container>
             </Navbar>
