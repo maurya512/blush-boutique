e4dd970dcc8f2686132be9186321ce8ec6fc8d43
maurya512 <patelmaurya0512@gmail.com>
2021-08-04 22:59:05 -0400

    created a datbase config file to establish connection to mongodb

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+// databse config file for mongoose
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+
+        console.log(`MONGO DB CONNECTED: ${conn.connection.host}`)
+    } catch(error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+}
+
+export default connectDB
